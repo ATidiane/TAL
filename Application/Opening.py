84195eb9025bb1fbe846c28e04177f878d4d0357
Ahmed Tidiane BALDE <baldeahmedtidiane36@gmail.com>
2018-05-11 08:35:21 +0200

    nan

--- Application/Opening.py
+++ Application/Opening.py
@@ -13,7 +13,7 @@ from tkinter.filedialog import askopenfilename
 class Opening(tkinter.Tk):
 
     def __init__(self, parent):
-        
+
         tkinter.Tk.__init__(self, parent)
         self.parent = parent
         self.title = "ExtractMWU"
@@ -22,77 +22,70 @@ class Opening(tkinter.Tk):
         self.bgButton = "#FF9800"
         self.main()
         self['bg'] = self.bgColor
-        
+
         self.mainloop()
-        
 
     def destroyFrame(self):
         for widget in self.winfo_children():
             widget.pack_forget()
             widget.place_forget()
-            
 
     def quitter(self):
         self.destroy()
-        
 
     def addLabel(self, s, text, side=TOP, font=("ObelixProBroken", 16)):
         l = Label(s, text=text, font=font, padx=10, pady=10,
-                     bg=self.bgColor, fg="white")
+                  bg=self.bgColor, fg="white")
         l.pack(side=side)
         return l
-        
 
     def addFrame(self, s, side=TOP):
         return Frame(s, relief=GROOVE, bg=self.bgColor).pack(side=side)
-        
 
-    def addButton(self, s, text, command, width=12, side=TOP, font=("Times", 15)):
-        return Button(s, text=text, command= command, font=font, padx=10, 
+    def addButton(self, s, text, command, width=12,
+                  side=TOP, font=("Times", 15)):
+        return Button(s, text=text, command=command, font=font, padx=10,
                       pady=10, bg=self.bgButton, width=width).pack(side=side)
-                      
 
     def addButtonPlace(self, s, text, command, width=12, relx=0.5, rely=0.5,
-                      anchor=CENTER, font=("Times", 15)):
-        b = Button(s, text=text, command=command, font=font, padx=10, 
-                      pady=10, bg=self.bgButton, width=width)
+                       anchor=CENTER, font=("Times", 15)):
+        b = Button(s, text=text, command=command, font=font, padx=10,
+                   pady=10, bg=self.bgButton, width=width)
         return b.place(relx=relx, rely=rely, anchor=anchor)
-        
-    
+
     def appendText(self, T, text):
         T.insert(END, text, 'color')
-    
-    
+
     def OpenFile(self):
-        fileName = askopenfilename(initialdir=".", 
-                    filetypes =(("Text File", "*.txt"),("All Files","*.*")), 
-                    title = "Choose a file.")
-        try:            
+        fileName = askopenfilename(initialdir=".",
+                                   filetypes=(("Text File", "*.txt"),
+                                              ("All Files", "*.*")),
+                                   title="Choose a file.")
+        try:
             self.fichier = open(fileName, 'r')
-            self.realWindow()            
+            self.realWindow()
         except FileNotFoundError:
             print("No such file")
-            
-            
+
     def realWindow(self):
         self.destroyFrame()
-        self.geometry("900x600")            
-        T = Text(self, height=40, width=70, borderwidth=0)            
+        self.geometry("900x600")
+        T = Text(self, height=40, width=70, borderwidth=0)
         scroll = Scrollbar(self, command=T.yview)
         T.configure(yscrollcommand=scroll.set)
-        T.tag_configure('color', foreground='#212121', 
-						font=('Times', 11, 'normal'))
+        T.tag_configure('color', foreground='#212121',
+                        font=('Times', 11, 'normal'))
         scroll.pack(side=RIGHT, fill=Y)
         T.pack(side=RIGHT)
         self.appendText(T, self.fichier.read())
-            
-    
+
     def main(self):
         self.addLabel(self, "Welcome\nto ExtractMWU")
         self.chooseF = self.addFrame(self, BOTTOM)
-        self.addButtonPlace(self.chooseF, "Choose a file", command=self.OpenFile)
-        self.addButtonPlace(self.chooseF, "Quit", command=self.quitter, rely=0.8)
-        
+        self.addButtonPlace(self.chooseF, "Choose a file",
+                            command=self.OpenFile)
+        self.addButtonPlace(self.chooseF, "Quit",
+                            command=self.quitter, rely=0.8)
 
 
 Opening(None)
--- Application/mwuhe.py
+++ Application/mwuhe.py
@@ -2,42 +2,44 @@
 
 import nltk
 import numpy as np
-# Pour changer le path du nltk_data, très très important 
-nltk.data.path.append("/Infos/nltk/nltk_data")
-
 from nltk.corpus import brown
+
+# Pour changer le path du nltk_data, très très important
+nltk.data.path.append("/Infos/nltk/nltk_data")
 
 
 def sents_categories(*categories):
     if not len(categories):
         return brown.sents(), brown.tagged_sents()
-    return brown.sents(categories=categories), brown.tagged_sents(tagset=categories)
+    return brown.sents(
+        categories=categories), brown.tagged_sents(
+        tagset=categories)
 
 
 def words_categories(*categories):
     if not len(categories):
         return brown.words(), brown.tagged_words()
-    return brown.words(categories=categories), brown.tagged_words(tagset=categories)
+    return brown.words(
+        categories=categories), brown.tagged_words(
+        tagset=categories)
 
 
 def ngrammes(words_text, n):
     """  Transforme le corpus en ngrams
     """
     ngrams, d = [], words_text
-    ngrams.append([[''.join(d[i:j]) for j in range(i+1,min(i+n+1,len(d)+1))]
-                            for i in range(len(d))])
-            
+    ngrams.append([[''.join(d[i:j]) for j in range(
+        i + 1, min(i + n + 1, len(d) + 1))] for i in range(len(d))])
+
     return ngrams
 
+
 def words_ngrams():
-    
+
     pass
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     words_text, tags_text = words_categories()
     all_ngrams = ngrammes(words_text, 2)
-    print(all_ngrams[0])        
-
-
-    
+    print(all_ngrams[0])
